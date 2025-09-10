7919e24e56ba24a1e9c09841cfde00031836dfda
adsfibonacci <speiglead@gmail.com>
2025-09-10 12:37:15 -0400

    fixed docstrings

--- kfolds.h
+++ kfolds.h
@@ -9,7 +9,6 @@
 #define KFOLDS_H
 
 /**
- * Add some working-dir branch info - oops
  * Return N-folds of indices to define the X_train/test and y_train/test
  * @param samples: should be the first dimension of the design matrix
  * @param k: the number of folds to make
